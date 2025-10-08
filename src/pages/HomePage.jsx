ebbc4dbfbb0967f556dc1baa960e67942f753000
kelechiibeleche <kelechibeleche@gmail.com>
2025-10-08 11:42:49 +0200

    css edit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export const HomePage = () => {
   return (
     <div className="about-container">
-      <h1>About Our Cocktail Page 🍸</h1>
+      <h1>Welcome to our cocktail page!🍸</h1>
 
       <section className="about-intro">
         <p>
-          <strong>Welcome to our cocktail page!</strong> Here you will find a
-          variety of cocktail recipes to try at home. This react based project
-          was built by two students exploring front-end development and trying
-          out concepts such as components, routing and API integration.
+          Here you will find a variety of cocktail recipes to try at home. This
+          react based project was built by two students exploring front-end
+          development and trying out concepts such as components, routing and
+          API integration.
         </p>
       </section>
 
@@ -18,14 +18,14 @@ export const HomePage = () => {
         <h2>Meet The Creators</h2>
 
         <div className="team-members">
-          <div className="member 1">
+          <div className="member">
             <h3>Fabian</h3>
             <p>Loves UI design and bringing creative ideas to life.</p>
             <a href="https://github.com/fabian-dietrich/m2-cocktail-project">
               Placeholder Fabians Github Account
             </a>
           </div>
-          <div className="member 2">
+          <div className="member">
             <h3>Kelechi</h3>
             <p>Enjoys coding with APIs and making smooth interactive apps.</p>
             <a href="https://github.com/fabian-dietrich/m2-cocktail-project">
